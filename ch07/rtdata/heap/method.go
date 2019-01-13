1d36eb2d243ecf1c3d4615b9165dfcdfe7ca821c
liam <liam@192.168.5.70>
2019-01-14 07:35:16 +0800

    add method invoke parameter parsse

--- ch07/rtdata/heap/method.go
+++ ch07/rtdata/heap/method.go
@@ -9,6 +9,7 @@ type Method struct {
 	maxStack uint
 	maxLocals uint
 	code []byte
+	argSlotCount uint
 }
 
 func newMethods(class *Class, cfMethods []*classfile.MemberInfo) []*Method {
@@ -18,6 +19,7 @@ func newMethods(class *Class, cfMethods []*classfile.MemberInfo) []*Method {
 		methods[i].class = class
 		methods[i].copyMemberInfo(cfMethod)
 		methods[i].copyAttributes(cfMethod)
+		methods[i].calArgSlotCount()
 	}
 
 	return methods
@@ -42,4 +44,25 @@ func (self *Method) MaxLocals() uint {
 
 func (self *Method) Code() []byte {
 	return self.code
+}
+
+func (self *Method) ArgSlotCount() uint {
+	return self.argSlotCount
+}
+
+func (self *Method) calArgSlotCount() uint {
+	parsedDescriptor := parseMethodDescriptor(self.descriptor)
+	for _, parameterType := range parsedDescriptor.parameterTypes {
+		self.argSlotCount++
+
+		//long或double类型占用2个slot
+		if parameterType == "J" || parameterType == "D" {
+			self.argSlotCount++
+		}
+	}
+
+	//实例方法额外增加this参数
+	if !self.IsStatic() {
+		self.argSlotCount++
+	}
 }
